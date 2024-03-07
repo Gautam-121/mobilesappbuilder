a777aeb78f458609cfb7917fcb81fa3814be5c5f
Samad Rahman~ <samadsrahman@gmail.com>
2024-03-07 12:45:11 +0530

    GET Api added to HomeTab

--- client/pages/debug/Data.jsx
+++ client/pages/debug/Data.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
 import Home from "../Home/Home";
 
 
 const GetData = () => {
+// const fetch = useFetch();
 
+// useEffect( async()=>{
+//   const res = await fetch("http://localhost:4000/apps/api/getHomePageByShop/BW")
+//   const data = await res.json();
+//   console.log(data)
+// },[])
 
   return (
     <>
